16d297008aae0f660856e03aa1e70c16a7cb44a4
Austin Ohlfs <ohlfsam96@gmail.com>
2023-10-25 11:56:06 -0600

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+       self.name = name
+       self.breed = breed
+
+    # def bark(self):
+    #     print("Woof!")
+
+    # def showing_self(self):
+    #     return self
+
+# fido = Dog("Fido")
+# # Fido is born!
+# fido.showing_self()
+# # <__main__.Dog object at 0x104f8bfa0>
+
+dog1 = Dog("Buddy", "Golden Retriever")
+dog2 = Dog ("Rex")
+
+print(f"{dog1.name} is a {dog1.breed}")
+print(f"{dog2.name} is a {dog2.breed}")
